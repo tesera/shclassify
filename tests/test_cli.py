355aace0e61157a62da0df544e0899c16f9985bd
Jotham Apaloo <jothamapaloo@gmail.com>
2016-03-08 20:44:11 -0800

    setup CLI and update test
    add to cli:
    - path argument
    - delim choice
    - intermediate predictions flag
    - verbose flag
    - still needs to be tied into console stream logger
    - add optional path for output file with callback for default - callback
    - not validated!
    
    Update cli test for work to this point
    
    also reformats cli usage log

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,8 +3,8 @@ from click.testing import CliRunner
 from shclassify.scripts.cli import cli
 
 
-def test_cli_count():
+def test_cli_exits_if_input_file_nonexistant():
     runner = CliRunner()
-    result = runner.invoke(cli, ['3'])
-    assert result.exit_code == 0
-    assert result.output == "False\nFalse\nFalse\n"
+    result = runner.invoke(cli, ['nopath'])
+    assert 'Path "nopath" does not exist' in result.output
+    assert result.exit_code != 0
